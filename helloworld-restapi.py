1260c80e4962ec01c72a7a206eb1ccf81c0f26b0
Krzysztofj12345 <krzysztofj12345@gmail.com>
2023-06-03 11:20:50 -0400

    Update helloworld-restapi.py

--- helloworld-restapi.py
+++ helloworld-restapi.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify(message='Hello, World Krzysztof!')
+    return jsonify(message='Hello, World Krzysztof: dockerhub!')
 
 if __name__ == '__main__':
     # app.run()
